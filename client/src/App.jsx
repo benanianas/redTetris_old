20fac300e318e395e80b62a36956674b702b8513
Mourad Meski <mmeski@e3r12p10.1337.ma>
2022-09-16 16:06:56 +0100

    Routing the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,34 @@
-import { useEffect } from "react";
-import socket from "../utils/socket";
+import React from "react";
+import { useParams, Routes, Route, BrowserRouter } from "react-router-dom";
 
-import "./App.css";
-
-function App() {
-  useEffect(() => {
-    socket.on("connect", () => {
-      console.log(socket.id);
-    });
-  }, []);
-}
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Login />} />
+            <Route path=":id" element={<Game />} />
+          </Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
+
+const Login = () => {
+  return <h1>Create a logine to play</h1>;
+};
+
+const Error = () => {
+  return <h1>Error Page</h1>;
+};
+
+const Game = () => {
+  let { id } = useParams();
+  console.log("from games", id);
+  return <h1>Create Room to play the games: {id}</h1>;
+};
